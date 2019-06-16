896ff8c42e6ec098c45ccf7ba94a47781ae5d91e
Moongun <mich.wiaz@gmail.com>
2019-06-16 23:53:06 +0200

    removed not used classes

--- src/DSL/DSLBundle/Form/PeriodicityType.php
+++ src/DSL/DSLBundle/Form/PeriodicityType.php
@@ -2,18 +2,12 @@
 
 namespace DSL\DSLBundle\Form;
 
-use PHPUnit\Framework\Constraint\LessThan;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Validator\Constraints\LessThanOrEqual;
-use Symfony\Component\Validator\Constraints\NotBlank;
-use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\Validator\Constraints\GreaterThan;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 class PeriodicityType extends AbstractType
