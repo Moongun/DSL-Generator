a83e1ac8b78796a23bb1528e7d05d0f9ee074717
Moongun <mich.wiaz@gmail.com>
2019-07-04 23:47:39 +0200

    dodanie przekierowania do metody pdf z generowaniem diety

--- src/DSL/DSLBundle/Controller/GeneratePdfController.php
+++ src/DSL/DSLBundle/Controller/GeneratePdfController.php
@@ -1,10 +1,22 @@
 <?php
 
-
 namespace DSL\DSLBundle\Controller;
 
+use DSL\DSLBundle\Entity\DietRules;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
-class GeneratePdfController
+/**
+ * @Route("/generate_pdf", name="generate_pdf_")
+ */
+class GeneratePdfController extends Controller
 {
+    /**
+     * @Route("/diet/{id}", name="diet")
+     * @param DietRules $dietRule
+     */
+    public function generateDiet(DietRules $dietRule)
+    {
 
+    }
 }
