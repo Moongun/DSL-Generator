e1464cd5337d3115a5ac10e4b3e9c18c24d9e185
Moongun <mich.wiaz@gmail.com>
2019-10-10 00:33:16 +0200

    annoations added

--- src/DSL/DSLBundle/Service/PdfGenerator.php
+++ src/DSL/DSLBundle/Service/PdfGenerator.php
@@ -10,17 +10,36 @@ class PdfGenerator
     private $knpSnappyPdf;
     private $pdfDir;
 
+    /**
+     * PdfGenerator constructor.
+     *
+     * @param Pdf $knpSnappyPdf SnappyPDF
+     * @param string $pdfDir    Path to pdf directory
+     */
     public function __construct(Pdf $knpSnappyPdf, string $pdfDir)
     {
         $this->knpSnappyPdf = $knpSnappyPdf;
         $this->pdfDir = $pdfDir;
     }
 
+    /**
+     * Create absolute path for file.
+     *
+     * @param string $fileName Filename
+     *
+     * @return string
+     */
     public function createPathToFile(string $fileName)
     {
         return $this->pdfDir . $fileName;
     }
 
+    /**
+     * Generate pdf file.
+     *
+     * @param string $html Html
+     * @param $pathToFile Path to file
+     */
     public function generate(string $html, $pathToFile)
     {
         $options = ['page-width' => 595];
@@ -28,6 +47,13 @@ class PdfGenerator
         return $this->knpSnappyPdf->generateFromHtml($html, $pathToFile, $options);
     }
 
+    /**
+     * Create file name.
+     *
+     * @param array $components Array with unique diet data
+     * @param string $extension Extension
+     * @return string
+     */
     public function createFileName(array $components, string $extension = 'pdf')
     {
         return md5(implode('_',$components)) . '.' . $extension;
